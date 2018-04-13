26d497ad28021d821e3b4c5de74043bfad45b796
stajicvladimir <vstajic@gmail.com>
2018-04-13 14:35:44 +0200

    coponents fun bla

--- assigment 6/Start/vue-cli/src/App.vue
+++ assigment 6/Start/vue-cli/src/App.vue
@@ -1,39 +1,30 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
+        <app-header></app-header>
         <hr>
         <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
+            <app-servers></app-servers>
+            <app-server-details></app-server-details>
         </div>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
+import Header from "./components/shared/Header.vue";
+import Footer from "./components/shared/Footer.vue";
+import Servers from "./components/server/Servers.vue";
+import ServerDetails from "./components/server/ServerDetails.vue";
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-footer': Footer,
+            'app-servers': Servers,
+            'app-server-details': ServerDetails
+        }
+    }
 </script>
 
 <style>
