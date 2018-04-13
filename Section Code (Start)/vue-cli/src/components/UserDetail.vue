046022e8e995add63cc15269cd6fd647cb321acd
stajicvladimir <vstajic@gmail.com>
2018-04-13 15:08:10 +0200

    still playing with components... sending data between them

--- Section Code (Start)/vue-cli/src/components/UserDetail.vue
+++ Section Code (Start)/vue-cli/src/components/UserDetail.vue
@@ -2,13 +2,30 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name: {{ myName }}</p>
+        <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="resetName">Reset name</button>  
+        <button @click="resetFn()">Reset name resetFn</button>  
     </div>
 </template>
 
 <script>
 export default {
-    props: ['myName']
+    props: {
+        myName: {
+            type: String
+        },
+        resetFn: Function,
+        userAge: Number
+    },
+    methods: {
+        switchName(){
+            return this.myName.split("").reverse().join("");
+        },
+        resetName(){
+            this.myName = 'Vladimir';
+        }
+    }
 }
 </script>
 
